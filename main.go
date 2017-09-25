fb31d7eb0616fbf6f6d468a1e642e4cbac07cbb1
robbybt <robby.budianto@tokopedia.com>
2017-09-25 15:58:32 +0700

    merge conflict

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main() {
 	// robbyfor.Forbiasa()
 	// robbyfor.Foreachbiasa()
 
-<<<<<<< HEAD
 	// // c := [3]int{}
 	// // c[0] = 1
 	// // c[1] = 2
@@ -29,19 +28,4 @@ func main() {
 	variable.Printapapun([]int{1, 2, 3})
 	variable.Printapapun(map[string]int{"satu": 1, "dua": 2})
 	variable.Ketiktodos()
-=======
-	// c := [3]int{}
-	// c[0] = 1
-	// c[1] = 2
-	// for _, v := range c {
-	// 	fmt.Println(v)
-	// }
-	a := robbyfor.Person{"a", "b"}
-	arr := []*robbyfor.Person{}
-	arr = append(arr, &a)
-	arr = append(arr, &robbyfor.Person{"a1", "b2"})
-	robbyfor.ForeachStruct(arr)
-	robbyfor.Whilebiasa()
-	fmt.Println("asd")
->>>>>>> fcda7e7a447c6166e8ade4d60cfd1cdfb752517f
 }
