49cd3b89a7661bdcc7f9e02e87ebc065a95651b9
robbybt <robby.budianto@tokopedia.com>
2017-09-25 10:48:29 +0700

    latihan for biasa

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"../golang/robbyfor"
+	//"fmt"
+)
+
+func main() {
+	robbyfor.Forbiasa()
+}
---
+++ robbyfor/for.go
@@ -0,0 +1,11 @@
+package robbyfor
+
+import (
+	"fmt"
+)
+
+func Forbiasa() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
